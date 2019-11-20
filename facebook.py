47cb289fd84010a180cade57842c2b1bef22b909
hiba21-meet <52972842+hiba21-meet@users.noreply.github.com>
2019-11-20 13:03:48 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,10 +1,13 @@
+posts=[]
+captions=[]
+
 class User():
 	def __init__(self,name,email,password):
 		self.name=name
 		self.email=email
 		self.password=password
 		self.friends_list=[]
-		self.posts=[]
+		self.messages_list=[]
 	def add_friend(self,email):
 		self.friends_list.append(email)
 		print(self.name + " has added " + email + " as a friend ")
@@ -12,14 +15,34 @@ class User():
 		self.friends_list.remove(email)
 		print(self.name +" has removed " + email + " from freinds ")
 	def create_post(self,text):
-		self.posts.append(text)
+		posts.append(text)
 		print(self.name + " has posted " + text)
 	def get_userInfo(self):
 		print("Name" + self.name )
 		print("Email" + self.email )
 		print("Password" + self.password)
 		print("freinds" + str(self.friends_list))
-		print("Posts" + str(self.posts))
+		print("Posts" + str(posts))
+	def send_message(self,email,text):
+		self.messages_list.append(email)
+
+class Post():
+	def __init__(self,name,caption,author ,comments=[]):
+		self.name=name
+		self.caption=caption 
+		self.comments=[]
+		self.author=author
+	def add_caption(self,caption_text):
+		self.caption_text=caption_text
+		captions.append(caption_text)
+		print(self.name + " has added a caption" + caption_text)
+	def add_comment(self, comment_text):
+		self.comment_text = comment_text
+		self.comments.append(comment_text)
+		print(self.name + " has added a comment: " + comment_text)
+	def like_post(self):
+		print(self.name +"has liked your post !")
+
 user1 = User(" Layan"," layann.hamdan@gmail.com"," layan123")
 user2 = User(" celine"," celine.yaghi@gmail.com", " ceine1234")
 user1.add_friend("celine.yaghi@gmail.com")
@@ -27,8 +50,6 @@ user2.create_post("happy birthday")
 user1.get_userInfo()
 user2.get_userInfo()
 user1.remove_friend("celine.yaghi@gmail.com")
-class Post():
-
-
-
-
+post1 = Post("layan","food is great","comment")
+post1.add_caption(" hi")
+user1.send_message("celine.yaghi@gmail.com","hi")
